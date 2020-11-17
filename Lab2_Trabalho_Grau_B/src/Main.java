23c937d0d50d892d738eafa7fbe5e0d347e00126
gustavo <gustavo@gustavo-Vostro-3460>
2020-11-17 10:26:20 -0300

    Criação da classe Task

--- Lab2_Trabalho_Grau_B/src/Main.java
+++ Lab2_Trabalho_Grau_B/src/Main.java
@@ -3,7 +3,19 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Menu.menu();
+		Task tarefa = new Task(2020,11,20, "fazer um bolo");
+		
+		System.out.println(tarefa.toString());
+		
+		tarefa.setReScheduledDateToAccomplish(2020, 11, 22);
+		
+		System.out.println("\n\n" + tarefa.toString());
+		
+		tarefa.setAccomplishedDate(2020, 11, 22);
+		
+		System.out.println("\n\n" + tarefa.toString());		
+		
+	
 		
 
 	}
